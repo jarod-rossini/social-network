42cceca7c511f1122b2d8b8b34250e5825aea8b3
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2021-06-09 14:05:14 +0200

    inscription : css, a faire : regex ajax password

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
     console.log('App is ready');
 
+
+    // REGEX PASSWORD
+    $( "#password" ).focus(function() {
+        $("#restriction").append("<li id=\"pwd-restriction-length\">Doit contenir au moins 8 charactères</li>\n" +
+            "<li id=\"pwd-restriction-upperlower\">Doit contenir au moins une lettre en majuscule et une lettre en minuscule</li>\n" +
+            "<li id=\"pwd-restriction-number\">Doit contenir au moins un chiffre (0–9)</li>\n" +
+            "<li id=\"pwd-restriction-special\">Doit contenir au moin sun charactère spécial (!@#$%^&()'[]\"?+-/*)</li>");
+    });
+    var pwdLength = /^.{8,255}$/;
+    var pwdUpper = /[A-Z]+/;
+    var pwdLower = /[a-z]+/;
+    var pwdNumber = /[0-9]+/;
+    var pwdSpecial = /[!@#$%^&()'[\]"?+-/*={}.,;:_]+/;
+
+
+
     document.getElementById('formreg').addEventListener('submit', (e) => {
         e.preventDefault();
         //  récupérer la valeur des input
@@ -8,43 +24,50 @@ $(document).ready(function(){
         let surname = document.getElementById('surname').value;
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
+        let cpass = document.getElementById('password_conf').value;
         let birthdate = document.getElementById('birthdate').value;
 
         const allowedEmailDomain = 'laplateforme.io';
         if (email.split('@')[1] === allowedEmailDomain) {
-            // do something, we accept this email
-            $.ajax({
-                url: 'app/controller/register.php',
-                type: 'POST',
-                data: {
-                    name: name,
-                    surname: surname,
-                    email: email,
-                    password:password,
-                    birthdate:birthdate
-                },
-                dataType: 'html',
+            if ( password === cpass) {
+                // do something, we accept this email
+                $.ajax({
+                    url: 'app/controller/register.php',
+                    type: 'POST',
+                    data: {
+                        name: name,
+                        surname: surname,
+                        email: email,
+                        password:password,
+                        birthdate:birthdate
+                    },
+                    dataType: 'html',
 
-                success: function (datatype){
-                    var success = datatype;
-                    if(success == 'vous êtes inscrit') {
-                        setTimeout(function(){alert("vous êtes inscrit");location.replace("index.php");},1000);
+                    success: function (datatype){
+                        var success = datatype;
+                        if(success == 'vous êtes inscrit') {
+                            setTimeout(function(){alert("vous êtes inscrit");location.replace("index.php");},1000);
+                        }
+                        else if (success == 'Email déjà prit') {
+                            alert("Email déjà enregistrer");
+                            $('#email').val('').focus();
+                            $('#password').val('');
+                        }
+                        else {
+                            alert('Veuillez remplir tous les champs');
+                        }
+                    },
+                    error: function (request, status, error) {
+                        console.log(request);
+                        console.log(status);
+                        console.log(error);
                     }
-                    else if (success == 'Email déjà prit') {
-                        alert("Email déjà enregistrer");
-                        $('#email').val('').focus();
-                        $('#password').val('');
-                    }
-                    else {
-                        alert('Veuillez remplir tous les champs');
-                    }
-                },
-                error: function (request, status, error) {
-                    console.log(request);
-                    console.log(status);
-                    console.log(error);
+                })
             }
-        })} else {
+            else {
+                alert('Mots de passes différents !');
+            }
+        } else {
             // return an error or do nothing
             alert('Vous devez vous connecté avec votre email de la plateforme ! ');
         }
--- view/inscription.view.php
+++ view/inscription.view.php
@@ -3,31 +3,46 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>Inscription</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
 		<link href="public/css/style.css" rel="stylesheet"/>
 	</head>
 	<body>
-		<main class="maininscription">
-			<form method='post' id="formreg">
-                <label for="name">Prénom</label>
-                <input type="text" name="name" id="name" maxlength="255" autofocus/> <br>
+        <main class="maininscription">
+            <img class="wave" src="public/image/wave3.png">
+            <div class="container-inscription">
+                <div class="img">
+                    <img src="public/image/fall-is-coming.svg">
+                </div>
+                <div class="reg-container">
+                    <form method='post' id="formreg">
+                        <img class="avatar" src="public/image/avatarfemale.svg"><br/>
 
-                <label for="surname">Nom</label>
-                <input type="text" name="surname" id="surname" maxlength="255"/> <br>
+                        <h2>Bienvenue</h2>
 
-                <label for="email">e-Mail</label>
-                <input type="email" name="email" id="email" maxlength="255"/> <br>
+                        <label for="name">Prénom</label>
+                        <input type="text" name="name" id="name" maxlength="255" autofocus/> <br>
 
-                <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password" maxlength="255"/> <br>
+                        <label for="surname">Nom</label>
+                        <input type="text" name="surname" id="surname" maxlength="255"/> <br>
 
-                <label for="password_conf">Confirmation</label>
-                <input type="password" name="password_conf" id="password_conf" maxlength="255"/> <br>
+                        <label for="email">e-Mail</label>
+                        <input type="email" name="email" id="email" maxlength="255"/> <br>
 
-                <label for="birthdate">Date de Naissance</label>
-                <input type="date" name="birthdate" id="birthdate"/> <br>
+                        <label for="password">Mot de passe</label>
+                        <input type="password" name="password" id="password" maxlength="255"/>
+                         <br>
 
-                <input class="submit" type="submit" id="register" value="Inscription"/>
-            </form>
+                        <label for="password_conf">Confirmation</label>
+                        <input type="password" name="password_conf" id="password_conf" maxlength="255"/> <br>
+
+                        <label for="birthdate">Date de Naissance</label>
+                        <input type="date" name="birthdate" id="birthdate"/> <br>
+
+                        <input class="submit" type="submit" id="register" value="Inscription"/>
+                    </form>
+                </div>
+            </div>
 
 
             <?php //var_dump($_SESSION);
